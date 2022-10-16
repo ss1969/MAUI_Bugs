05fc1d60b01d6c25e3e532ce75404b5b21519bfd
ss1969 <kiilii@hotmail.com>
2022-10-17 00:57:44 +0800

    first commit

---
+++ Bug_GridCollectionView/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Bug_GridCollectionView;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Bug_GridCollectionView/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Bug_GridCollectionView;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<string> Somethinguseless { get; set; }
+    public MainPage()
+	{
+        // place this line before InitializeComponent() will fail, move it after InitializeComponent(); will NOT;
+        // use a Viewmodel from constructor by Dependce-Injection in mauiprogram.cs will fail also; In this sample, I removed Viewmodels to reduce codes.
+        Somethinguseless = new ObservableCollection<string> { "", "" };
+        InitializeComponent();
+	}
+
+}
+
